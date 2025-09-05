8e62363e65b504ff1381e9fc21fdb83a9a3b255a
Yinze Rong <1851501842@qq.com>
2025-09-05 20:41:14 +0800

    uwsgi: extra more time

--- start.sh
+++ start.sh
@@ -10,9 +10,9 @@ uwsgi --module=BackEnd.wsgi:application \
     --master \
     --http=0.0.0.0:${PORT} \
     --processes=5 \
-    --harakiri=60 \
+    --harakiri=6000 \
     --max-requests=5000 \
     --vacuum \
     --buffer-size=65535 \
     --http-keepalive \
-    --http-timeout=60
+    --http-timeout=6000
